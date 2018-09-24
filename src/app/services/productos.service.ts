d728c8109ebd49ac6fe7f1c3cdf1058e3f690099
Jose D'Oleo <jdoleo@gmail.com>
2018-09-23 22:57:49 -0400

    Fin de la seccion

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interface';
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,16 +10,52 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor(private http: HttpClient) {
     this.cargarProductos();
   }
 
   private cargarProductos() {
-    this.http.get('https://angular-html-f6acd.firebaseio.com/productos_idx.json')
-        .subscribe( (resp: Producto[] ) => {
-        this.productos = resp;
-        this.cargando = false;
-        });
+
+    return new Promise( ( resolve, reject ) => {
+      this.http.get('https://angular-html-f6acd.firebaseio.com/productos_idx.json')
+      .subscribe( (resp: Producto[] ) => {
+      this.productos = resp;
+      this.cargando = false;
+      resolve();
+      });
+    } );
+  }
+
+  getProducto( id: string) {
+    return this.http.get(`https://angular-html-f6acd.firebaseio.com/productos/${ id }.json`);
+  }
+
+  buscarProducto( termino: string ) {
+
+    if ( this.productos.length === 0 ) {
+      // Cargar productos
+      this.cargarProductos().then( () => {
+        // Ejecutar despues de tener los productos
+        // Aplicar filtro
+        this.filtrarProductos( termino );
+      } );
+    } else {
+      // Aplicar filtro
+      this.filtrarProductos( termino );
+    }
+  }
+
+  filtrarProductos(termino: string) {
+    this.productosFiltrado = [];
+    termino = termino.toLowerCase();
+
+    this.productos.forEach(prod => {
+      const TITULO_LOWER = prod.titulo.toLowerCase();
+      if ( prod.categoria.indexOf( termino ) >= 0 || TITULO_LOWER.indexOf( termino ) >= 0 ) {
+        this.productosFiltrado.push( prod ); // agrego el elemento al arreglo
+      }
+    });
   }
 }
